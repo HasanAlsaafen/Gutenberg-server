007b92a5cf91b40105da9fc8ade14400b4f204f7
HasanAlsaafen <hasansaafen1234@gmail.com>
2025-08-11 17:06:44 +0300

    Fix

--- Services/Implementaions/ServiceService.cs
+++ Services/Implementaions/ServiceService.cs
@@ -1,27 +1,65 @@
-﻿using Gutenburg_Server.Models;
-using Gutenburg_Server.Repositories;
-
+﻿using Gutenburg_Server.DTOs;
+using Gutenburg_Server.Models;
+using Microsoft.EntityFrameworkCore;
+using Gutenburg_Server.Data;
 namespace Gutenburg_Server.Services
 {
     public class ServiceService : IServiceService
     {
-        private readonly IServiceRepository _repo;
+        private readonly AppDbContext _context;
 
-        public ServiceService(IServiceRepository repo)
+        public ServiceService(AppDbContext context)
         {
-            _repo = repo;
+            _context = context;
         }
 
-        public async Task<IEnumerable<Service>> GetAllAsync() => await _repo.GetAllAsync();
+        public async Task<IEnumerable<Service>> GetAllAsync()
+        {
+            return await _context.Services.ToListAsync();
+        }
 
-        public async Task<Service?> GetByIdAsync(int id) => await _repo.GetByIdAsync(id);
+        public async Task<Service?> GetByIdAsync(int id)
+        {
+            return await _context.Services.FindAsync(id);
+        }
 
         public async Task<Service> CreateAsync(Service service)
-            => await _repo.AddAsync(service);
+        {
+            _context.Services.Add(service);
+            await _context.SaveChangesAsync();
+            return service;
+        }
 
         public async Task<Service> UpdateAsync(Service service)
-            => await _repo.UpdateAsync(service);
+        {
+            _context.Services.Update(service);
+            await _context.SaveChangesAsync();
+            return service;
+        }
 
-        public async Task<bool> DeleteAsync(int id) => await _repo.DeleteAsync(id);
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var entity = await _context.Services.FindAsync(id);
+            if (entity == null)
+                return false;
+
+            _context.Services.Remove(entity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task CreateAsync(ServiceDTO dto)
+        {
+            var service = new Service
+            {
+                Title = dto.Title,
+                Description = dto.Description,
+                Image =dto.Image
+
+            };
+
+            _context.Services.Add(service);
+            await _context.SaveChangesAsync();
+        }
     }
 }
